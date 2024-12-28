6abbe8f3f3966f051febda88405ded821add4c32
juanchoalcoba <juanchoalcoba@gmail.com>
2024-12-27 23:49:11 -0300

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,24 +6,28 @@ function HomePage() {
     <div className="relative flex flex-col justify-center items-center w-full min-h-screen">
       {/* Imagen de fondo */}
       <div
-        className="absolute top-0 left-0 w-full h-full bg-no-repeat opacity-90 bg-cover z-[-1]"
+        className="absolute top-0 left-0 w-full h-full bg-no-repeat opacity-90 bg-cover z-[-2]"
         style={{ backgroundImage: `url(${heroImage})` }}
       ></div>
 
+      {/* Overlay con opacidad para dar un efecto más elegante al hero */}
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 z-[-1]"></div>
+
+      {/* Contenido */}
       <h1 className="text-4xl flex text-center flex-col font-extrabold sm:text-4xl p-4 text-red-800">
         Bienvenidos a <span className="text-3xl sm:text-4xl p-4">Party</span>{' '}
         <span className="text-3xl sm:text-4xl p-4 text-cyan-950">Kids</span>
       </h1>
       <p className="text-center font-extrabold text-[20px]">Reserva tu Party DAY!!!</p>
 
-      {/* Enlace con la etiqueta Link para ir a la página de reservas */}
+      {/* Botones */}
       <Link to="/booking">
-        <button className="bg-gray-400 py-2 px-8 font-bold rounded-full mt-4">
+        <button className="bg-yellow-900 text-white hover:bg-orange-950 hover:text-yellow-300 transition-all py-2 px-8 font-bold rounded-full mt-4">
           Reservar
         </button>
       </Link>
       <Link to="/private">
-        <button className="bg-gray-400 py-2 px-8 font-bold rounded-full mt-4">
+        <button className="border border-1 border-black py-2 px-8 font-bold rounded-full mt-4">
           Panel Privado
         </button>
       </Link>
