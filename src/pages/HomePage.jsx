8dc590f5aa265eb04181950bf35e0d3aa6e65092
juanchoalcoba <juanchoalcoba@gmail.com>
2024-12-28 01:05:10 -0300

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,13 @@
-import { Link } from "react-router-dom";
-import heroImage from "../assets/hero.jpg"; // Asegúrate de que la ruta sea correcta
+import { Link } from 'react-router-dom';
+import heroImage from '../assets/hero.jpg'; // Asegúrate de que la ruta sea correcta
 
 function HomePage() {
   return (
     <div className="relative flex flex-col justify-center items-center w-full min-h-screen">
       {/* Imagen de fondo */}
       <div
-        className="absolute top-0 left-0 w-full aspect-[16/9] h-full bg-no-repeat bg-center opacity-80 bg-auto z-[-1]"
-        style={{
-          backgroundImage: `url(${heroImage})`,
-        }}
+        className="absolute top-0 left-0 w-full aspect-w-16 aspect-h-9 h-full bg-no-repeat bg-center  opacity-80 bg-auto z-[-1]"
+        style={{ backgroundImage: `url(${heroImage})` }}
       ></div>
 
       {/* Overlay con opacidad para dar un efecto más elegante al hero */}
@@ -18,12 +16,10 @@ function HomePage() {
       {/* Contenedor con fondo blur */}
       <div className="relative z-10 bg-white/30 backdrop-blur-sm p-8 rounded-2xl shadow-lg max-w-xl text-center">
         <h1 className="text-4xl flex text-center flex-col font-extrabold sm:text-4xl p-4 text-red-800">
-          Bienvenidos a <span className="text-3xl sm:text-4xl p-4">Party</span>{" "}
+          Bienvenidos a <span className="text-3xl sm:text-4xl p-4">Party</span>{' '}
           <span className="text-3xl sm:text-4xl p-4 text-cyan-950">Kids</span>
         </h1>
-        <p className="text-center font-extrabold text-[20px]">
-          Reserva tu Party DAY!!!
-        </p>
+        <p className="text-center font-extrabold text-[20px]">Reserva tu Party DAY!!!</p>
 
         {/* Botones */}
         <Link to="/booking">
