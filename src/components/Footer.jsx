4c96d61b8bfad0078a2a6fab5b88d208f0a8e969
juanchoalcoba <juanchoalcoba@gmail.com>
2025-01-04 14:02:19 -0300

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,8 +14,8 @@ const Footer = () => {
 
         {/* Links adicionales */}
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4  text-center md:text-left text-sm mb-4 md:mb-0">
-          <Link to="/about" className="hover:text-blue-50 transition duration-300 text-[13px]">Sobre Nosotros</Link>
-          <Link to="/services" className="hover:text-blue-50 transition duration-300 text-[13px]">Términos</Link>
+          <Link to="/about" className="hover:text-blue-50 transition duration-300 text-[11px]">Sobre Nosotros</Link>
+          <Link to="/services" className="hover:text-blue-50 transition duration-300 text-[11px]">Términos</Link>
           <Link to="/contact" className="hover:text-blue-50 transition duration-300 text-[11px]">Contacto</Link>
         </div>
       </div>
