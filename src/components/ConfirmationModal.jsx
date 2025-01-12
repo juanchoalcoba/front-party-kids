f48cf3bb9d6f28f77ceab85976cbd68b359f6454
juanchoalcoba <juanchoalcoba@gmail.com>
2025-01-12 02:02:46 -0300

    update

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -4,12 +4,12 @@ const ConfirmationModal = ({ show, onClose, onConfirm, bookingData }) => {
   return (
     <div>
       {/* Overlay oscuro */}
-      <div className="fixed inset-0 bg-black opacity-80 z-40"></div>
+      <div className="fixed inset-0 bg-black opacity-30 z-40"></div>
 
       {/* Modal */}
       <div className="fixed inset-0 z-50 flex items-center justify-center">
-        <div className="bg-white p-6 rounded-lg shadow-lg max-w-[23rem] w-full relative z-50">
-          <h2 className="text-lg font-bold mb-4">¿Estás seguro de confirmar la reserva?</h2>
+        <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full relative z-50">
+          <h2 className="text-xl font-bold mb-4">¿Estás seguro de confirmar la reserva?</h2>
           <div className="mb-4 space-y-2">
             <p><strong>Tu Nombre:</strong> {bookingData.name}</p>
             <p><strong>Nombre del Niño/a:</strong> {bookingData.namekid}</p>
--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import CalendarComponent from '../components/CalendarComponent';
-import Modal from '../components/Modal'; // Importamos el modal para mostrar mensajes
-import ConfirmationModal from '../components/ConfirmationModal'; // Modal para confirmar reserva
+import Modal from '../components/Modal'; // Importamos el modal
 
 const BookingPage = () => {
   const [bookingData, setBookingData] = useState({
@@ -11,11 +10,10 @@ const BookingPage = () => {
     phone: '',
     date: new Date(),
   });
-
-  const [showModal, setShowModal] = useState(false); // Estado para mostrar/ocultar modal de confirmación de reserva
-  const [showConfirmationModal, setShowConfirmationModal] = useState(false); // Estado para mostrar/ocultar modal de confirmación antes de enviar
-  const [modalMessage, setModalMessage] = useState(''); // Mensaje en el modal
   
+  const [showModal, setShowModal] = useState(false); // Estado para mostrar/ocultar modal
+  const [modalMessage, setModalMessage] = useState(''); // Mensaje en el modal
+
   const handleChange = (e) => {
     setBookingData({ ...bookingData, [e.target.name]: e.target.value });
   };
@@ -24,8 +22,8 @@ const BookingPage = () => {
     setBookingData({ ...bookingData, date });
   };
 
-  const handleConfirmSubmit = async () => {
-    // Aquí hacemos el envío final de los datos al backend
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const response = await fetch('https://api-party-kids.vercel.app/api/bookings', {
       method: 'POST',
       headers: {
@@ -36,18 +34,11 @@ const BookingPage = () => {
 
     if (response.ok) {
       setModalMessage('Reserva completada, nos pondremos en contacto a la brevedad');
-      setShowConfirmationModal(false); // Cerrar modal de confirmación
-      setShowModal(true); // Mostrar modal de éxito
+      setShowModal(true); // Mostrar modal si la reserva fue exitosa
     } else {
       setModalMessage('Error submitting the booking');
-      setShowConfirmationModal(false); // Cerrar modal de confirmación
-      setShowModal(true); // Mostrar modal de error
+      setShowModal(true); // Mostrar modal en caso de error
     }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowConfirmationModal(true); // Mostrar modal de confirmación
   };
 
   const closeModal = () => {
@@ -55,90 +46,84 @@ const BookingPage = () => {
     window.location.href = '/'; // Redirige a la página de inicio (opcional)
   };
 
-  const closeConfirmationModal = () => {
-    setShowConfirmationModal(false);
-  };
-
   return (
     <div className="p-8 flex flex-col justify-center items-center bg-gradient-to-r from-violet-950 via-purple-600 to-blue-500 w-full min-h-screen font-robert-medium">
+
+
+
+
+      
       <h1 className="text-3xl mb-6 font-bold text-center text-blue-50">¡Completa el formulario para registrar tu fiesta!</h1>
       
       <form onSubmit={handleSubmit} className="space-y-6 bg-white p-8 rounded-xl shadow-xl w-full sm:w-96">
-        <div className="flex flex-col">
-          <label htmlFor="name" className="text-gray-700 font-semibold mb-2">Tu Nombre</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Escribe tu nombre y apellido"
-            value={bookingData.name}
-            onChange={handleChange}
-            className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
-            required
-          />
-        </div>
+  <div className="flex flex-col">
+    <label htmlFor="name" className="text-gray-700 font-semibold mb-2">Tu Nombre</label>
+    <input
+      type="text"
+      id="name"
+      name="name"
+      placeholder="Escribe tu nombre y apellido"
+      value={bookingData.name}
+      onChange={handleChange}
+      className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
+      required
+    />
+  </div>
 
-        <div className="flex flex-col">
-          <label htmlFor="namekid" className="text-gray-700 font-semibold mb-2">Nombre del Niño/a</label>
-          <input
-            type="text"
-            id="namekid"
-            name="namekid"
-            placeholder="Escribe el nombre del niño/a"
-            value={bookingData.namekid}
-            onChange={handleChange}
-            className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
-            required
-          />
-        </div>
+  <div className="flex flex-col">
+    <label htmlFor="namekid" className="text-gray-700 font-semibold mb-2">Nombre del Niño/a</label>
+    <input
+      type="text"
+      id="namekid"
+      name="namekid"
+      placeholder="Escribe el nombre del niño/a"
+      value={bookingData.namekid}
+      onChange={handleChange}
+      className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
+      required
+    />
+  </div>
 
-        <div className="flex flex-col">
-          <label htmlFor="email" className="text-gray-700 font-semibold mb-2">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Escribe tu email"
-            value={bookingData.email}
-            onChange={handleChange}
-            className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
-            required
-          />
-        </div>
+  <div className="flex flex-col">
+    <label htmlFor="email" className="text-gray-700 font-semibold mb-2">Email</label>
+    <input
+      type="email"
+      id="email"
+      name="email"
+      placeholder="Escribe tu email"
+      value={bookingData.email}
+      onChange={handleChange}
+      className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
+      required
+    />
+  </div>
 
-        <div className="flex flex-col">
-          <label htmlFor="phone" className="text-gray-700 font-semibold mb-2">Teléfono</label>
-          <input
-            type="tel"
-            id="phone"
-            name="phone"
-            placeholder="Escribe tu número de teléfono"
-            value={bookingData.phone}
-            onChange={handleChange}
-            className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
-            required
-          />
-        </div>
+  <div className="flex flex-col">
+    <label htmlFor="phone" className="text-gray-700 font-semibold mb-2">Teléfono</label>
+    <input
+      type="tel"
+      id="phone"
+      name="phone"
+      placeholder="Escribe tu número de teléfono"
+      value={bookingData.phone}
+      onChange={handleChange}
+      className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
+      required
+    />
+  </div>
 
-        <div className="flex flex-col">
-          <label className="text-gray-700 font-semibold mb-2">Selecciona la Fecha</label>
-          <CalendarComponent onDateChange={handleDateChange} />
-        </div>
+  <div className="flex flex-col">
+    <label className="text-gray-700 font-semibold mb-2">Selecciona la Fecha</label>
+    <CalendarComponent onDateChange={handleDateChange} />
+  </div>
 
-        <button type="submit" className="bg-cyan-600 hover:bg-pink-700 text-white font-bold p-3 rounded-lg transition-all duration-300 w-full">
-          Confirmar
-        </button>
-      </form>
+  <button type="submit" className="bg-cyan-600 hover:bg-pink-700 text-white font-bold p-3 rounded-lg transition-all duration-300 w-full">
+    Confirmar
+  </button>
+</form>
 
-      {/* Modal de confirmación de reserva */}
-      <ConfirmationModal 
-        show={showConfirmationModal} 
-        onClose={closeConfirmationModal} 
-        onConfirm={handleConfirmSubmit} 
-        bookingData={bookingData} 
-      />
 
-      {/* Mostrar modal de resultado final */}
+      {/* Mostrar modal */}
       <Modal show={showModal} onClose={closeModal} message={modalMessage} />
     </div>
   );
