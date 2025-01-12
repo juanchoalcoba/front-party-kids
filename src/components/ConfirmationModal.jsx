74a1574f9d5ad0958701bb5b1b4090bbd15ad4cd
juanchoalcoba <juanchoalcoba@gmail.com>
2025-01-12 01:51:36 -0300

    update

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -8,7 +8,7 @@ const ConfirmationModal = ({ show, onClose, onConfirm, bookingData }) => {
 
       {/* Modal */}
       <div className="fixed inset-0 z-50 flex items-center justify-center">
-        <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full relative z-50">
+        <div className="bg-white p-6 rounded-lg shadow-lg max-w-[23rem] w-full relative z-50">
           <h2 className="text-xl font-bold mb-4">¿Estás seguro de confirmar la reserva?</h2>
           <div className="mb-4 space-y-2">
             <p><strong>Tu Nombre:</strong> {bookingData.name}</p>
