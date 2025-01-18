5b8a26e792237bf1b5b610a523ce832e775a5125
juanchoalcoba <juanchoalcoba@gmail.com>
2025-01-18 09:50:24 -0300

    update

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -16,6 +16,8 @@ const ConfirmationModal = ({ show, onClose, onConfirm, bookingData }) => {
             <p><strong>Email:</strong> {bookingData.email}</p>
             <p><strong>Teléfono:</strong> {bookingData.phone}</p>
             <p><strong>Fecha de la Fiesta:</strong> {bookingData.date.toLocaleDateString()}</p>
+            <p><strong>Fecha de la Fiesta:</strong> {bookingData.hours}</p>
+            <p><strong>Fecha de la Fiesta:</strong> {bookingData.timeSlot}</p>
           </div>
           <div className="flex justify-between">
             <button
