cec457125375f766889fda147f6a73a0217170ac
juanchoalcoba <juanchoalcoba@gmail.com>
2025-01-21 12:03:03 -0300

    update

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -114,13 +114,73 @@ const BookingPage = () => {
       <h1 className="text-3xl mb-6 font-bold text-center text-blue-50">¡Completa el formulario para registrar tu fiesta!</h1>
       
       <form onSubmit={handleSubmit} className="space-y-6 bg-white p-8 rounded-xl shadow-xl w-full sm:w-96">
-        {/* Aquí van todos los campos del formulario */}
+
+      <div className="flex flex-col">
+          <label htmlFor="name" className="text-gray-700 font-semibold mb-2">Tu Nombre</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Escribe tu nombre y apellido"
+            value={bookingData.name}
+            onChange={handleChange}
+            className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
+            required
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="namekid" className="text-gray-700 font-semibold mb-2">Nombre del Niño/a</label>
+          <input
+            type="text"
+            id="namekid"
+            name="namekid"
+            placeholder="Escribe el nombre del niño/a"
+            value={bookingData.namekid}
+            onChange={handleChange}
+            className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
+            required
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="email" className="text-gray-700 font-semibold mb-2">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Escribe tu email"
+            value={bookingData.email}
+            onChange={handleChange}
+            className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
+            required
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="phone" className="text-gray-700 font-semibold mb-2">Teléfono</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            placeholder="Escribe tu número de teléfono"
+            value={bookingData.phone}
+            onChange={handleChange}
+            className="border-2 border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-pink-300 focus:outline-none p-3 w-full rounded-lg"
+            required
+            minLength="8" // Mínimo de 8 caracteres
+            maxLength="9" // Máximo de 9 caracteres
+            pattern="^\d{8,9}$" // Acepta solo números con 8 o 9 dígitos
+            title="El número debe tener entre 8 y 9 dígitos"
+          />
+        </div>
+
         <div className="flex flex-col">
           <label className="text-gray-700 font-semibold mb-2">Selecciona la Fecha</label>
           <CalendarComponent onDateChange={handleDateChange} bookedHours={bookedHours} />
         </div>
 
-        {/* Otros campos como nombre, email, etc. */}
+       
 
         {/* Campo para seleccionar duración de horas */}
         <div className="flex flex-col">
