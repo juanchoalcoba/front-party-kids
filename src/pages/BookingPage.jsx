7b704e2f807b4865bd8a0952079c53d083567db6
juanchoalcoba <juanchoalcoba@gmail.com>
2025-01-22 07:30:10 -0300

    update

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -67,37 +67,27 @@ const BookingPage = () => {
     setShowConfirmationModal(false);
   };
 
-  const generateStartTimes = (bookedTimes) => {
+  const generateStartTimes = () => {
     const times = [];
-    let startHour = 8; // Comienza a las 8:00 AM
-    const maxStartHourFor4Hours = 20; // Última hora para 4 horas (8 PM)
-    const maxStartHourFor8Hours = 16; // Última hora para 8 horas (4 PM)
-    
-    const isTimeBooked = (timeSlot) => {
-      return bookedTimes.some(time => time === timeSlot);
-    };
+    let startHour = 8; // Empezamos a las 8:00 AM
+    const maxStartHourFor4Hours = 20; // Última hora de inicio válida para 4 horas (20:00)
+    const maxStartHourFor8Hours = 16; // Última hora de inicio válida para 8 horas (16:00)
   
     if (bookingData.hours === '4') {
+      // Para 4 horas: desde las 8 AM hasta las 8 PM (último posible inicio a las 8 PM)
       while (startHour <= maxStartHourFor4Hours) {
-        const timeSlot = `${startHour}:00`;
-        if (!isTimeBooked(timeSlot)) { 
-          times.push(timeSlot);
-        }
+        times.push(`${startHour}:00`);
         startHour++;
       }
     } else if (bookingData.hours === '8') {
+      // Para 8 horas: desde las 8 AM hasta las 4 PM (último posible inicio a las 4 PM)
       while (startHour <= maxStartHourFor8Hours) {
-        const timeSlot = `${startHour}:00`;
-        if (!isTimeBooked(timeSlot)) { 
-          times.push(timeSlot);
-        }
+        times.push(`${startHour}:00`);
         startHour++;
       }
     }
-  
     return times;
   };
-  
 
   return (
     <div className="p-8 flex flex-col justify-center items-center bg-gradient-to-r from-violet-950 via-purple-600 to-blue-500 w-full min-h-screen font-robert-medium">
