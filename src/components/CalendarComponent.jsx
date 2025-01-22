8ed718c35ac02be18d989174ee95da0e631dad28
juanchoalcoba <juanchoalcoba@gmail.com>
2025-01-22 09:29:36 -0300

    update

--- src/components/CalendarComponent.jsx
+++ src/components/CalendarComponent.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 import '../App.css'
 
-const CalendarComponent = ({ onDateChange, bookedSlots }) => {
+const CalendarComponent = ({ onDateChange }) => {
   const [date, setDate] = useState(new Date());
+  const [bookedDates, setBookedDates] = useState([]); // Guardar fechas reservadas
   const today = new Date(); // Fecha de hoy
+
+  // Función para obtener fechas reservadas desde el backend
+  const fetchBookedDates = async () => {
+    try {
+      const response = await fetch('https://api-party-kids.vercel.app/api/bookings');
+      const data = await response.json();
+      // Mapeamos solo las fechas de las reservas
+      const booked = data.map(booking => new Date(booking.date));
+      setBookedDates(booked);
+    } catch (error) {
+      console.error('Error fetching booked dates:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBookedDates(); // Llamada cuando el componente se monta
+  }, []);
+
+  // Función que maneja el cambio de fecha
+  const handleDateChange = (newDate) => {
+    setDate(newDate);
+    onDateChange(newDate);
+  };
 
   // Función para deshabilitar las fechas anteriores al día actual
   const disableDates = ({ date, view }) => {
@@ -20,8 +44,8 @@ const CalendarComponent = ({ onDateChange, bookedSlots }) => {
   // Función para agregar una clase a las fechas reservadas y deshabilitadas
   const tileClassName = ({ date, view }) => {
     if (view === 'month') {
-      // Resaltar las fechas reservadas en amarillo usando bookedSlots
-      if (bookedSlots.some(bookedDate => bookedDate.toDateString() === date.toDateString())) {
+      // Resaltar las fechas reservadas en amarillo
+      if (bookedDates.some(bookedDate => bookedDate.toDateString() === date.toDateString())) {
         return 'booked-date'; // Clase CSS para las fechas reservadas
       }
       // Deshabilitar las fechas anteriores a hoy
@@ -30,12 +54,6 @@ const CalendarComponent = ({ onDateChange, bookedSlots }) => {
       }
     }
     return '';
-  };
-
-  // Función que maneja el cambio de fecha
-  const handleDateChange = (newDate) => {
-    setDate(newDate);
-    onDateChange(newDate);
   };
 
   return (
