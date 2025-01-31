218914e14653c02fafa73945b93b3ba306932782
juanchoalcoba <juanchoalcoba@gmail.com>
2025-01-30 22:28:14 -0300

    update

--- src/components/CalendarComponent.jsx
+++ src/components/CalendarComponent.jsx
@@ -72,6 +72,7 @@ const CalendarComponent = ({ onDateChange, onBookingDataChange }) => {
 
   const tileClassName = ({ date, view }) => {
     if (view === "month") {
+      // Verifica si ya existe una reserva o si el día ya está bloqueado
       if (
         bookedDates.some(
           (booking) => booking.date.toDateString() === date.toDateString()
@@ -79,12 +80,20 @@ const CalendarComponent = ({ onDateChange, onBookingDataChange }) => {
       ) {
         return "booked-date";
       }
+      // Verifica si la fecha está en el pasado
       if (date < today) {
         return "disabled-date";
+      }
+  
+      // Verifica si no hay horas disponibles para 4 o 8 horas para este día
+      const availableTimes = generateStartTimes(date); // Pasa la fecha aquí si lo necesitas
+      if (availableTimes.length === 0) {
+        return "no-available-time"; // Añadimos esta clase para marcar el día en rojo
       }
     }
     return "";
   };
+  
 
   const generateStartTimes = () => {
     const times = [];
