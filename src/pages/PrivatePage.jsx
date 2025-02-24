82f122a294a82995b53c3171bc41cde4c2e2b166
juanchoalcoba <juanchoalcoba@gmail.com>
2025-02-24 13:12:31 -0300

    update

--- src/pages/PrivatePage.jsx
+++ src/pages/PrivatePage.jsx
@@ -121,8 +121,8 @@ const PrivatePage = () => {
   const confirmedBookings = bookings.filter((booking) => booking.confirmed);
 
   return (     
-    <div className="bg-gray-900 min-h-screen p-6">       
-      <div className="max-w-6xl mx-auto bg-gray-800 shadow-xl rounded-lg p-8 border-4 border-violet-400">         
+    <div className="bg-gray-900 min-h-screen p-6 w-full">       
+      <div className="max-w-6xl mx-auto bg-gray-800 shadow-xl w-full rounded-lg p-4 border-4 border-violet-400">         
         <h2 className="text-3xl font-bold text-center text-white mb-6">Reservas Pendientes</h2>         
         <div className="overflow-x-auto">           
           <table className="min-w-full table-auto bg-gray-800 text-white shadow-md rounded-lg text-sm">             
