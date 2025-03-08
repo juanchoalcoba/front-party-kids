9b258f753d6a0dbc33a525de481c54948f38d749
juanchoalcoba <juanchoalcoba@gmail.com>
2025-03-08 19:18:21 -0300

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
                 className='relative z-10 h-dvh w-screen aspect-video overflow-hidden rounded-lg bg-black'
             >
                 <video
-                    src="videos/herovideo.mp4"
+                    src="videos/herohd.mp4"
                     ref={videoRef}
                     autoPlay
                     loop
