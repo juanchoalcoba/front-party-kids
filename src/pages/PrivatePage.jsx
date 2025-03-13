de81ff43c6367d398d53b7148e3690c4f3ffbc83
juanchoalcoba <juanchoalcoba@gmail.com>
2025-03-13 09:46:51 -0300

    update

--- src/pages/PrivatePage.jsx
+++ src/pages/PrivatePage.jsx
@@ -106,7 +106,7 @@ const PrivatePage = () => {
     }
   };
 
-  const handleViewed = async (name) => {
+  const handleViewed = async (date) => {
     const viewed = window.confirm(
       "¿Estás seguro de que quieres marcar esta reserva como leída?"
     );
@@ -120,7 +120,7 @@ const PrivatePage = () => {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ name }), // Enviamos el 'name' en el cuerpo de la solicitud
+          body: JSON.stringify({ date }), // Enviamos el 'name' en el cuerpo de la solicitud
         }
       );
 
