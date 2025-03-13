26047da0cb01791dbac45b30a72581867c2395eb
juanchoalcoba <juanchoalcoba@gmail.com>
2025-03-13 11:15:34 -0300

    update

--- src/pages/PrivatePage.jsx
+++ src/pages/PrivatePage.jsx
@@ -106,34 +106,35 @@ const PrivatePage = () => {
     }
   };
 
-  const handleViewed = async (_id) => {
+  const handleViewed = async (name) => {
     const viewed = window.confirm(
       "¿Estás seguro de que quieres marcar esta reserva como leída?"
     );
     if (!viewed) return;
-  
+
     try {
       const response = await fetch(
-        `https://api-party-kids.vercel.app/api/bookings/${_id}`,  // Ruta de la API
+        `https://api-party-kids.vercel.app/api/bookings`,
         {
-          method: "PUT",
+          method: "PATCH",
           headers: {
             "Content-Type": "application/json",
           },
+          body: JSON.stringify({ name }), // Enviamos el 'name' en el cuerpo de la solicitud
         }
       );
-  
+
       if (!response.ok) {
         const data = await response.json();
         throw new Error(
           data.message || "Error al marcar la reserva como leída"
         );
       }
-  
+
       // Si la confirmación es exitosa, actualizamos el estado
       setBookings((prevBookings) =>
         prevBookings.map((booking) =>
-          booking._id === _id ? { ...booking, viewedByAdmin: true } : booking
+          booking.name === name ? { ...booking, viewedByAdmin: true } : booking
         )
       );
       alert("Reserva marcada como leída con éxito");
@@ -142,7 +143,6 @@ const PrivatePage = () => {
       alert("Hubo un error al intentar marcar la reserva como leída");
     }
   };
-  
 
   // Función para agrupar reservas por mes
   const groupBookingsByMonth = (bookings) => {
@@ -247,7 +247,7 @@ const PrivatePage = () => {
                         <input
                           type="checkbox"
                           checked={booking.viewedByAdmin}
-                          onChange={() => handleViewed(booking._id)}
+                          onChange={() => handleViewed(booking.name)}
                           disabled={booking.viewedByAdmin}
                         />
                       </td>
