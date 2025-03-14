253bfa26adc861154eb0cc21e6a80829b8c4e48e
juanchoalcoba <juanchoalcoba@gmail.com>
2025-03-13 22:38:30 -0300

    update

--- src/pages/PrivatePage.jsx
+++ src/pages/PrivatePage.jsx
@@ -195,15 +195,15 @@ const PrivatePage = () => {
     }, {});
   };
 
-  // Filtrar reservas pendientes y confirmadas
-  const pendingBookings = bookings.filter((booking) => !booking.confirmed);
-  const confirmedBookings = bookings.filter((booking) => booking.confirmed);
-  const archivedBookings = bookings.filter((booking) => booking.archived);
+// Filtrar reservas pendientes, confirmadas y archivadas
+const pendingBookings = bookings.filter((booking) => !booking.confirmed && !booking.archived);
+const confirmedBookings = bookings.filter((booking) => booking.confirmed && !booking.archived);
+const archivedBookings = bookings.filter((booking) => booking.archived);
 
-  // Agrupar reservas pendientes y confirmadas por mes
-  const groupedPendingBookings = groupBookingsByMonth(pendingBookings);
-  const groupedConfirmedBookings = groupBookingsByMonth(confirmedBookings);
-  const groupedArchivedBookings = groupBookingsByMonth(archivedBookings);
+// Agrupar reservas pendientes, confirmadas y archivadas por mes
+const groupedPendingBookings = groupBookingsByMonth(pendingBookings);
+const groupedConfirmedBookings = groupBookingsByMonth(confirmedBookings);
+const groupedArchivedBookings = groupBookingsByMonth(archivedBookings);
 
   // Autenticación
   if (!authenticated) {
