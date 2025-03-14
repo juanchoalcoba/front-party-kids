dde5b02ac4fc2975fb29289a38be379bdf08d26e
juanchoalcoba <juanchoalcoba@gmail.com>
2025-03-14 07:23:18 -0300

    update

--- src/pages/PrivatePage.jsx
+++ src/pages/PrivatePage.jsx
@@ -409,7 +409,7 @@ const PrivatePage = () => {
                         </button>
                         <button
                           onClick={() => handleArchived(booking._id)}
-                          className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300"
+                          className="bg-red-500 text-white px-4 py-2 ml-4 rounded-lg hover:bg-red-600 transition duration-300"
                         >
                           Archivar
                         </button>
