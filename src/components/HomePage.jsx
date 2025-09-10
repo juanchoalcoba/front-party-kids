387f5eac073fe7a994d41f73e017c4b80f7f36c2
juanchoalcoba <juanchoalcoba@gmail.com>
2025-09-09 21:30:25 -0300

    update

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,67 +1,84 @@
-import {useState, useEffect } from 'react';
-import heroImage from '../assets/futbolito.jpg'; // Asegúrate de que la ruta sea correcta
-import Button from '../components/Button';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import heroImage from "../assets/futbolito.jpg"; // Asegúrate de que la ruta sea correcta
+import Button from "../components/Button";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Link } from "react-router-dom";
 import { SiTiktok } from "react-icons/si";
-import { FiChevronDown } from "react-icons/fi";
-
+import {
+  FaBirthdayCake,
+  FaChurch,
+  FaGlassCheers,
+  FaBriefcase,
+} from "react-icons/fa";
 
 function HomePage() {
-
-
   useEffect(() => {
     AOS.init({ duration: 1000 });
     AOS.refresh(); // Esto asegura que las animaciones se recalculen cuando sea necesario
   }, []);
 
   return (
-    <div id="inicio" className="relative flex flex-col justify-center items-center w-full min-h-screen">
+    <div
+      id="inicio"
+      className="relative flex flex-col justify-center items-center w-full min-h-screen"
+    >
       {/* Imagen de fondo */}
       <div
         className="absolute top-0 left-0 w-full h-full bg-no-repeat bg-center opacity-80 bg-cover z-[-1]"
         style={{ backgroundImage: `url(${heroImage})` }}
       ></div>
 
-    
       {/* Título con degradado llamativo */}
-
-
-
 
       {/* Overlay con opacidad para dar un efecto más elegante al hero */}
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-[-2]"></div>
 
       {/* Contenedor con fondo blur */}
-      <div 
+      <div
         data-aos="fade-up"
         className="relative flex flex-col justify-center items-center z-10 bg-white/40 backdrop-blur-md font-robert-medium p-8 rounded-2xl shadow-lg max-w-xl text-center"
       >
         <h1 className="text-2xl text-cyan-950 flex flex-col items-center justify-center text-center font-robert-regular font-bold md:text-3xl p-4">
-  Bienvenidos/as 
-  <img 
-    className="w-[170px] rounded-full p-4 shadow-md shadow-black"
-    src="logo.png" alt="Logo" 
-  />
-</h1>
-      <p className='text-gray-950 text-md '>Cumpleaños - Bautismos</p>
-      <p className='text-gray-950 text-md'>Fiestas privadas - Eventos </p>
+          Bienvenidos/as
+          <img
+            className="w-[130px] rounded-full p-4 shadow-md shadow-black"
+            src="logo.png"
+            alt="Logo"
+          />
+        </h1>
+        <div className="flex justify-center items-start flex-col text-gray-950 text-md mt-4">
+          <div className="flex items-center gap-2">
+            <FaBirthdayCake className="text-pink-600" />
+            <span>Cumpleaños</span>
+          </div>
 
-       
+          <div className="flex items-center gap-2">
+            <FaChurch className="text-blue-600" />
+            <span>Bautismos</span>
+          </div>
 
-        <p className="text-center text-cyan-950 font-bold font-circular-web mb-4 pt-4 text-[20px]">
+          <div className="flex items-center gap-2">
+            <FaGlassCheers className="text-yellow-600" />
+            <span>Fiestas privadas</span>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <FaBriefcase className="text-green-600" />
+            <span>Eventos</span>
+          </div>
+        </div>
+
+        <p className="text-center text-cyan-950 font-bold font-circular-web mb-2 pt-4 text-[20px]">
           CONSULTAR FECHA
         </p>
 
-      
-
         {/* Botón RESERVAS */}
         <Link to="booking">
-          <Button 
+          <Button
             id="watch-trailer"
             title={
-              <button className='text-[15px] text-black font-general text-center flex items-center justify-center'>
+              <button className="text-[15px] text-black font-general text-center flex items-center justify-center">
                 RESERVAS
               </button>
             }
@@ -70,30 +87,28 @@ function HomePage() {
         </Link>
 
         {/* Sección de iconos centrados debajo del botón */}
-        <div className="flex justify-center gap-8 mt-8">
-        <Link target="_blank" to="https://www.tiktok.com/@kids.party.pdlt?_t=ZM-8tXN5ZUyAMf&_r=1">
-      <SiTiktok className="text-gray-800 text-4xl hover:text-gray-900 transition-all duration-300" />
-    </Link>
-  
-  <Link to="https://www.instagram.com/kidsparty.pdlt?igsh=MWt0eTdpMTdvdDBiMQ=="
-  target='_blank'
-  >
-    <i className="fab fa-instagram text-pink-700 text-4xl hover:text-pink-900 transition-all duration-300"></i>
-  </Link>
-  
-  <Link  
-  target='_blank'
-  to="https://wa.me/59898015337">
-    <i className="fab fa-whatsapp text-green-700 text-4xl hover:text-green-900 transition-all duration-300"></i>
-</Link>
-    </div>
+        <div className="flex justify-center gap-8 mt-4">
+          <Link
+            target="_blank"
+            to="https://www.tiktok.com/@kids.party.pdlt?_t=ZM-8tXN5ZUyAMf&_r=1"
+          >
+            <SiTiktok className="text-gray-800 text-4xl hover:text-gray-900 transition-all duration-300" />
+          </Link>
 
+          <Link
+            to="https://www.instagram.com/kidsparty.pdlt?igsh=MWt0eTdpMTdvdDBiMQ=="
+            target="_blank"
+          >
+            <i className="fab fa-instagram text-pink-700 text-4xl hover:text-pink-900 transition-all duration-300"></i>
+          </Link>
 
-
-
+          <Link target="_blank" to="https://wa.me/59898015337">
+            <i className="fab fa-whatsapp text-green-700 text-4xl hover:text-green-900 transition-all duration-300"></i>
+          </Link>
+        </div>
       </div>
     </div>
-  );      
+  );
 }
 
 export default HomePage;
