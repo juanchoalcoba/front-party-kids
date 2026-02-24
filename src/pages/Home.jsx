924b7dee35b7e8492f526fac7929b0b55b6302d2
juanchoalcoba <juanchoalcoba@gmail.com>
2026-02-24 19:32:17 -0300

    change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,23 @@
-import Features from "../components/Features";
+/* import Features from "../components/Features";
 import Hero from "../components/Hero";
 import HomePage from "../components/HomePage";
 import HomePage2 from "../components/HomePage2";
 import Navbar from "../components/Navbar";
 import AboutUs from '../components/Objetivo'
-import ImageSlider from "../components/Slide";
+import ImageSlider from "../components/Slide"; */
 
 function Home() {
   return (
       <main className='relative min-h-screen w-screen overflow-x-hidden'>
-        <Navbar />
+        <h1 className="text-center text-4xl text-white">Sitio momentaneamente fuera de servicio</h1>
+        {/* <Navbar />
         <Hero />
         <AboutUs />
         <HomePage />
         <ImageSlider />
         <Features />
-        <HomePage2 />
+        <HomePage2 /> */}
       </main>
-
   );
 }
 
