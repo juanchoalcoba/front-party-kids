5f3c24b26fc8053b44f7f33f9cfe97894503e41f
juanchoalcoba <juanchoalcoba@gmail.com>
2026-02-25 10:00:10 -0300

    change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,21 @@
-/* import Features from "../components/Features";
+import Features from "../components/Features";
 import Hero from "../components/Hero";
 import HomePage from "../components/HomePage";
 import HomePage2 from "../components/HomePage2";
 import Navbar from "../components/Navbar";
 import AboutUs from '../components/Objetivo'
-import ImageSlider from "../components/Slide"; */
+import ImageSlider from "../components/Slide";
 
 function Home() {
   return (
       <main className='relative min-h-screen w-screen overflow-x-hidden'>
-        <h1 className="text-center text-4xl text-white">Este sitio esta por caducar</h1>
-        {/* <Navbar />
+        <Navbar />
         <Hero />
         <AboutUs />
         <HomePage />
         <ImageSlider />
         <Features />
-        <HomePage2 /> */}
+        <HomePage2 />
       </main>
   );
 }
